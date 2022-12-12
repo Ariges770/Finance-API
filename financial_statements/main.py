b3c55c9533bb93ecd4a5bd31f18ea5945a9ab0a6
ariges770 <ariges770@gmail.com>
2022-12-12 13:42:17 +1100

    Removed commented out code

--- financial_statements/main.py
+++ financial_statements/main.py
@@ -2,24 +2,6 @@ from financial_statements.helpers import Results, load_pages, prepare_template, 
 
 
 def get_statement(statement_type: str, ticker: str, years_of_data: int):
-    # if statement_type == "is":
-    #     statement_type = "income-statement"
-    # elif statement_type == "cfs":
-    #     statement_type = "cash-flow"
-    # elif statement_type == "bs":
-    #     statement_type = "balance-sheet"
-    # else:
-    #     return {"Code": 400, "Statement Type": "Incorrect"}
-          
-        
-    # max_pages = get_max_pages(years_of_data)
-    # results = [None] * max_pages
-    # thread_list = []
-    # statement = {}
-    # statement["Ticker"] = ticker.upper()
-    # statement["Statement Type"] = statement_type    
-    # statement["Years Of Data"] = 0
-    # statement["Yearly Data"] = []
     
     results = Results(years_of_data, ticker, statement_type)
     
